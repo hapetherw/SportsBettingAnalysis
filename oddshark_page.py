18aa32c20d133179cebb24a5386725897b963b5d
harald <huis860@outlook.com>
2020-02-21 06:31:05 +0700

    change chrome webdriver part for server environment

--- oddshark_page.py
+++ oddshark_page.py
@@ -26,13 +26,13 @@ from config import SERVER_ENVIRONMENT
 
 if SERVER_ENVIRONMENT:
     chrome_options = Options()
-    # chrome_options.add_argument('--headless')
-    # chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--disable-dev-shm-usage')
-    # chrome_options.binary_location = '/usr/bin/google-chrome'
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.binary_location = '/usr/bin/google-chrome'
 
     chrome_driver = '/usr/bin/chromedriver'
-    browser = webdriver.Chrome(chrome_driver)
+    browser = webdriver.Chrome(executable_path=chrome_driver, options=chrome_options)
 else:
     browser = webdriver.Chrome('chromedriver.exe')
 browser.maximize_window()
